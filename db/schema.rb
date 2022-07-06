9d9d33d79094c492c5944f6029016e77a47930c4
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2022-07-07 08:24:40 +0900

    add: Gameモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_06_030610) do
+ActiveRecord::Schema.define(version: 2022_07_06_230955) do
 
   create_table "comics", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2022_07_06_030610) do
     t.integer "serialization_end_year"
     t.string "publisher"
     t.string "genre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "status"
+    t.string "result"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
