678344fbc4b62afb364aa9b9cb82d517793d5f56
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2022-07-06 09:06:11 +0900

    add: rails generateの設定を追加

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,12 @@ module RailsAkinator
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+			g.template_engine :slim
+			g.helper false      # ヘルパーを生成しない
+			g.assets false      # CSS, JavaScript ファイルを生成しない
+			g.skip_routes true  # config/routes.rb を変更しない
+			g.test_framework false
+		end
   end
 end
