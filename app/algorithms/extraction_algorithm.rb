898e3d2fef4822b87aa98e840aec91793987bf9e
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2022-07-07 11:54:22 +0900

    add: 連載中かどうかでの絞り込みアルゴリズムを追加

--- app/algorithms/extraction_algorithm.rb
+++ app/algorithms/extraction_algorithm.rb
@@ -13,10 +13,28 @@ class ExtractionAlgorithm
       question = progress.question
 
       # 絞り込み処理
+      case question.algorithm
+      when 'serialization_end'
+        serializtion_end?(progress)
+      else
+        raise Exception("invalid algorithm. --> #{question.algorithm}")
+      end
 
       Rails.logger.debug("On the way query is #{@query.to_sql}")
       Rails.logger.debug("On the way comics are #{@query.pluck(:title).to_a}")
     end
     @query
   end
+
+  private
+
+    def serializtion_end?(progress)
+      if progress.positive_answer?
+        @query = @query.where.not(serialization_end_year: nil)
+      end
+
+      if progress.negative_answer?
+        @query = @query.where(serialization_end_year: nil)
+      end
+    end
 end
