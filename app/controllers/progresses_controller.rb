a52a3af64d7aa5772a1a1d38f3726765a3908765
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2022-07-07 10:18:46 +0900

    add: 回答内容を保存する処理

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -5,5 +5,19 @@ class ProgressesController < ApplicationController
   end
 
   def create
+    current_game = Game.find(params[:game_id])
+
+    # 回答した内容を保存
+    progress = current_game.progresses.new(create_params)
+    progress.assign_sequence
+    progress.save!
+
+    redirect_to new_game_progress_path(current_game)
   end
+
+  private
+
+    def create_params
+      params.require(:progress).permit(%i[question_id answer])
+    end
 end
--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,4 +1,15 @@
 class Progress < ApplicationRecord
   belongs_to :game
   belongs_to :question
+
+  def assign_sequence
+    next_sequence = 1
+    return unless game.present?
+
+    all_progress = game.progresses
+    if all_progress.count.positive?
+      next_sequence = all_progress.maximum(:sequence) + 1
+    end
+    self.sequence = next_sequence
+  end
 end
